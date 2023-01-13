c6a37a6c00b2fba1171cb7423e77d8b032483706
enpitsulin <enpitsulin@gmail.com>
2023-01-13 15:54:21 +0800

    chore: move to monorepo and rename project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Milkdown-js
+
+Unofficial plugins collection repo
+
+| name                             | description |
+| -------------------------------- | ----------- |
+| `@milkdown-js/plugin-fullscreen` | none        |
--- example/src/main.ts
+++ example/src/main.ts
@@ -1,27 +1,31 @@
-import { fullscreen, ToggleFullscreen } from 'milkdown-plugin-fullscreen'
+import { fullscreen, ToggleFullscreen } from '@milkdown-js/plugin-fullscreen'
 import { Editor, rootCtx, commandsCtx } from '@milkdown/core'
 import { nord } from '@milkdown/theme-nord'
 import { commonmark } from '@milkdown/preset-commonmark'
 
-const root = document.querySelector('#app')
+async function main() {
+  const root = document.querySelector('#app')
 
-const editor = await Editor.make()
-  .config((ctx) => {
-    ctx.set(rootCtx, root)
+  const editor = await Editor.make()
+    .config((ctx) => {
+      ctx.set(rootCtx, root)
+    })
+    .use(nord)
+    .use(commonmark)
+    .use(fullscreen)
+    .create()
+
+  const button = document.createElement('button')
+  button.innerHTML = `fullscreen`
+  button.addEventListener('click', () => {
+    editor.action((ctx) => {
+      const commandManager = ctx.get(commandsCtx)
+
+      commandManager.call(ToggleFullscreen)
+    })
   })
-  .use(nord)
-  .use(commonmark)
-  .use(fullscreen)
-  .create()
 
-const button = document.createElement('button')
-button.innerHTML = `fullscreen`
-button.addEventListener('click', () => {
-  editor.action((ctx) => {
-    const commandManager = ctx.get(commandsCtx)
+  root?.append(button)
+}
 
-    commandManager.call(ToggleFullscreen)
-  })
-})
-
-root?.append(button)
+main()
