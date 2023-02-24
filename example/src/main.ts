27aa9a3bbbf7805a44438d106f69ab95f1fafba3
enpitsulin <enpitsulin@gmail.com>
2023-02-25 00:01:18 +0800

    refactor: rebuild example with basic editor

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,41 +1,19 @@
-import { fullscreen } from '@milkdown-lab/plugin-fullscreen'
-import { splitEditing } from '@milkdown-lab/plugin-split-editing'
 import { defaultValueCtx, Editor, rootCtx } from '@milkdown/core'
-import { defaultConfig, menu, menuPlugin } from '@milkdown/plugin-menu'
 import { commonmark } from '@milkdown/preset-commonmark'
-import { overrideNord } from './theme'
+
+import { nord } from '@milkdown/theme-nord'
+import '@milkdown/theme-nord/style.css'
 
 async function main() {
   const root = document.querySelector('#app')
 
   const editor = await Editor.make()
+    .config(nord)
     .config((ctx) => {
       ctx.set(rootCtx, root)
-      ctx.set(defaultValueCtx, '# Hello milkdown')
+      ctx.set(defaultValueCtx, '# Hello milkdown-lab')
     })
-    .use(overrideNord)
     .use(commonmark)
-    .use(fullscreen)
-    .use(
-      menu.configure(menuPlugin, {
-        config: [
-          ...defaultConfig,
-          [
-            {
-              type: 'button',
-              icon: 'splitEditing',
-              key: 'ToggleSplitEditing',
-            },
-            {
-              type: 'button',
-              icon: 'fullscreen',
-              key: 'ToggleFullscreen',
-            },
-          ],
-        ],
-      }),
-    )
-    .use(splitEditing)
     .create()
 }
 
