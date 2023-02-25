0272b0942465016de66e597e1d0ab6d2cf325568
enpitsulin <enpitsulin@gmail.com>
2023-02-25 19:30:31 +0800

    feat: add a button to toggle split editing view

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,6 +1,6 @@
 import { fullscreen, fullscreenOptionsCtx } from '@milkdown-lab/plugin-fullscreen'
-import { splitEditing } from '@milkdown-lab/plugin-split-editing'
-import { defaultValueCtx, Editor, rootCtx } from '@milkdown/core'
+import { splitEditing, toggleSplitEditing } from '@milkdown-lab/plugin-split-editing'
+import { commandsCtx, defaultValueCtx, Editor, rootCtx } from '@milkdown/core'
 import { commonmark } from '@milkdown/preset-commonmark'
 import { gfm } from '@milkdown/preset-gfm'
 
@@ -10,13 +10,19 @@ import './style.css'
 
 async function main() {
   const root = document.querySelector('#app')
-
+  const button = document.createElement('button')
+  button.innerText = 'hide split editing view'
+  button.addEventListener('click', () => {
+    editor.ctx.get(commandsCtx).call(toggleSplitEditing.key)
+  })
+  root?.appendChild(button)
   const editor = await Editor.make()
     .config(nord)
     .config((ctx) => {
+      // ctx.set(splitEditingOptionsCtx.key, { lineNumber: false })
       ctx.set(fullscreenOptionsCtx.key, { attributes: { class: 'fullscreen' } })
       ctx.set(rootCtx, root)
-      ctx.set(defaultValueCtx, '# Hello milkdown-lab\n1')
+      ctx.set(defaultValueCtx, '# Hello milkdown-lab')
     })
     .use(commonmark)
     .use(gfm)
