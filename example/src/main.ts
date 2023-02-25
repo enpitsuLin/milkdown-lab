7c8a396e43f8d754528d42ba15740bfba4b03623
enpitsulin <enpitsulin@gmail.com>
2023-02-25 16:48:51 +0800

    feat: add to example

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,9 +1,12 @@
-import { fullscreen } from '@milkdown-lab/plugin-fullscreen'
+import { fullscreen, fullscreenOptionsCtx } from '@milkdown-lab/plugin-fullscreen'
+import { splitEditing } from '@milkdown-lab/plugin-split-editing'
 import { defaultValueCtx, Editor, rootCtx } from '@milkdown/core'
 import { commonmark } from '@milkdown/preset-commonmark'
+import { gfm } from '@milkdown/preset-gfm'
 
 import { nord } from '@milkdown/theme-nord'
 import '@milkdown/theme-nord/style.css'
+import './style.css'
 
 async function main() {
   const root = document.querySelector('#app')
@@ -11,10 +14,13 @@ async function main() {
   const editor = await Editor.make()
     .config(nord)
     .config((ctx) => {
+      ctx.set(fullscreenOptionsCtx.key, { attributes: { class: 'fullscreen' } })
       ctx.set(rootCtx, root)
-      ctx.set(defaultValueCtx, '# Hello milkdown-lab')
+      ctx.set(defaultValueCtx, '# Hello milkdown-lab\n1')
     })
     .use(commonmark)
+    .use(gfm)
+    .use(splitEditing)
     .use(fullscreen)
     .create()
 }
