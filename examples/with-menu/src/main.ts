d1490708c7ce0a39a858ce747fbf6912c762cb7f
enpitsulin <enpitsulin@gmail.com>
2023-02-26 19:35:51 +0800

    chore: update example-with-menu as dev

--- examples/with-menu/src/main.ts
+++ examples/with-menu/src/main.ts
@@ -1,6 +1,5 @@
 import { menu, menuDefaultConfig } from '@milkdown-lab/plugin-menu'
-import { toggleSplitEditing } from '@milkdown-lab/plugin-split-editing'
-import { commandsCtx, defaultValueCtx, Editor, rootCtx } from '@milkdown/core'
+import { defaultValueCtx, Editor, rootCtx } from '@milkdown/core'
 import { commonmark } from '@milkdown/preset-commonmark'
 import { gfm } from '@milkdown/preset-gfm'
 
@@ -10,12 +9,6 @@ import './style.css'
 
 async function main() {
   const root = document.querySelector('#app')
-  const button = document.createElement('button')
-  button.innerText = 'hide split editing view'
-  button.addEventListener('click', () => {
-    editor.ctx.get(commandsCtx).call(toggleSplitEditing.key)
-  })
-  root?.appendChild(button)
   const editor = await Editor.make()
     .config(nord)
     .config(menuDefaultConfig)
