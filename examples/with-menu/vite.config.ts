c505e40fb919d5075defded70e40f01e4b04d6ad
enpitsulin <enpitsulin@gmail.com>
2023-02-27 15:50:34 +0800

    feat: improved example

---
+++ examples/with-menu/vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import Unocss from 'unocss/vite'
+import { presetUno, presetTypography, transformerDirectives } from 'unocss'
+
+export default defineConfig({
+  plugins: [Unocss({ presets: [presetUno(), presetTypography()], transformers: [transformerDirectives()] })],
+})
