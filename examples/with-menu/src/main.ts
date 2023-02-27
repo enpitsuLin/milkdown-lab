64f6294d37b18272b04f97b1beec2e24f7dd76e7
enpitsulin <enpitsulin@gmail.com>
2023-02-27 15:50:34 +0800

    feat: improved example

--- examples/with-menu/src/main.ts
+++ examples/with-menu/src/main.ts
@@ -2,9 +2,10 @@ import { menu, menuDefaultConfig } from '@milkdown-lab/plugin-menu'
 import { defaultValueCtx, Editor, rootCtx } from '@milkdown/core'
 import { commonmark } from '@milkdown/preset-commonmark'
 import { gfm } from '@milkdown/preset-gfm'
-
 import { nord } from '@milkdown/theme-nord'
+
 import '@milkdown/theme-nord/style.css'
+import 'uno.css'
 import './style.css'
 
 async function main() {
