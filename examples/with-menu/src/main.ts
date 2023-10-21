6e36e64d7e9ce4dfdbe82c17d0487fcfa74ed490
enpitsulin <enpitsulin@gmail.com>
2023-10-21 11:07:21 +0800

    chore: add histroy plugin to example for test

--- examples/with-menu/src/main.ts
+++ examples/with-menu/src/main.ts
@@ -3,6 +3,7 @@ import { defaultValueCtx, Editor, rootCtx } from '@milkdown/core'
 import { commonmark } from '@milkdown/preset-commonmark'
 import { gfm } from '@milkdown/preset-gfm'
 import { nord } from '@milkdown/theme-nord'
+import { history } from '@milkdown/plugin-history'
 
 import '@milkdown/theme-nord/style.css'
 import 'uno.css'
@@ -21,6 +22,7 @@ async function main() {
     .use(menu)
     .use(commonmark)
     .use(gfm)
+    .use(history)
     .create()
 }
 
